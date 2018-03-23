543b0e605db366a6be942c4c478d115b9da8f9fd
Patty8909 <patty90_30@hotmail.com>
2018-03-23 12:08:35 -0500

    con sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ uglify = require('gulp-uglify'),
 concat = require('gulp-concat');
 babel = require('gulp-babel');
 minifycss = require('gulp-minify-css');
+sass = require('gulp-sass')
 
 gulp.task('js', function () {
 return gulp.src('src/js/*.js')
@@ -13,8 +14,16 @@ return gulp.src('src/js/*.js')
 });
 
 gulp.task('css',function() {
-  return gulp.src('src/css/*.css')
+  return gulp.src(['src/css/*.css','src/sass/*.scss'])
+    .pipe(sass())
     .pipe(concat('main.min.css'))
     .pipe(minifycss())
     .pipe(gulp.dest('dist/css/'))
+})
+
+gulp.task('minifica-todo',['css','js'])
+
+gulp.task('watch', function(){
+  gulp.watch(['src/css/*.css', 'src/sass/*.scss'], ['css']);
+  gulp.watch('src/js/*.js', ['js']);
 })
